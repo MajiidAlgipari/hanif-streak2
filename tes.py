06e4aba5cd44352db9f80948dfc657f7ee1cb11e
MajiidAlgipari <146962413+MajiidAlgipari@users.noreply.github.com>
2026-08-03 09:51:23 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -11,7 +11,7 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 
 # 📝 Daftar nama tampilan teman Anda
 DAFTAR_TEMAN = [
-    "chell (^..^)9",
+    "chell",
     "Yann",
     "hottest إلمو",
     "Z",
